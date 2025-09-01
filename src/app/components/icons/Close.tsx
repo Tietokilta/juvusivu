45f6ffed4ecd11fd46afe91c431712c754c439c5
IiroP <iiro.poutiainen@gmail.com>
2025-09-01 22:40:11 +0300

    feat: update window icons and event loading bars
    - Window icons are now from Figma
    - Unreleased event loading bar now changes based on date

---
+++ src/app/components/icons/Close.tsx
@@ -0,0 +1,24 @@
+const CloseIcon = ({ size = 24 }: { size?: number }) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox="0 0 29 27"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M3.07178 3.77328L25.4107 23.3199"
+      stroke="#091F43"
+      strokeWidth="4.18855"
+      strokeLinecap="square"
+    />
+    <path
+      d="M25.4106 3.77328L3.0717 23.3199"
+      stroke="#091F43"
+      strokeWidth="4.18855"
+      strokeLinecap="square"
+    />
+  </svg>
+);
+
+export default CloseIcon;
---
+++ src/app/components/icons/Maximize.tsx
@@ -0,0 +1,24 @@
+const MaximizeIcon = ({ size = 24 }: { size?: number }) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox="0 0 26 27"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M23.8657 3.07565V24.018H2.92334V3.07565H23.8657Z"
+      fill="#C4CFE1"
+      stroke="#091F43"
+      strokeWidth="4.18855"
+    />
+    <path
+      d="M3.62061 6.56566H23.1672"
+      stroke="#091F43"
+      strokeWidth="4.18855"
+      strokeLinecap="round"
+    />
+  </svg>
+);
+
+export default MaximizeIcon;
