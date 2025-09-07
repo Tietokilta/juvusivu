bf25d332c80315b138508ba084bd6c503a765393
IiroP <iiro.poutiainen@gmail.com>
2025-09-07 19:34:58 +0300

    feat: better 404 page
    - Introduce stylized 404 error page

--- src/app/[locale]/[...path]/not-found.tsx
+++ src/app/[locale]/[...path]/not-found.tsx
@@ -1,6 +1,7 @@
 import { getI18n } from "@locales/server";
 import Header from "@components/Header";
 import { Window } from "@components/Window";
+import WarningIcon from "@components/icons/Warning";
 
 export default async function NotFound() {
   const t = await getI18n();
@@ -14,8 +15,11 @@ export default async function NotFound() {
         className="min-w-full py-10"
       />
       <main className="container mx-auto max-w-5xl px-8 py-8 sm:max-w-[90dvw]">
-        <Window title={t("page-not-found")} className="mx-auto max-w-[500px]">
-          <p className="mb-4">{t("page-not-found")}</p>
+        <Window title={t("error")} className="mx-auto max-w-[500px]">
+          <div className="font-pixel flex items-center space-x-4 p-4 text-xl">
+            <WarningIcon size={48} />
+            <p className="ml-4">{t("page-not-found")}</p>
+          </div>
         </Window>
       </main>
     </>
