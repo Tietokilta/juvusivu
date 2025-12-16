5f9384466d8c9357b6938ea202d74ad816bc7f23
lanttu1243 <lasse.kukkula@gmail.com>
2025-12-16 19:44:10 +0200

    Clean up code

--- src/app/[locale]/(with_layout)/page.tsx
+++ src/app/[locale]/(with_layout)/page.tsx
@@ -1,22 +1,11 @@
-import { getPayload } from "payload";
-import configPromise from "@payload-config";
-import { LexicalSerializer } from "@components/lexical/LexicalSerializer";
-import { getCurrentLocale } from "@locales/server";
 import Header from "@components/Header";
+import MainPage from "@components/MainPage";
 
 export default async function Home() {
-  const payload = await getPayload({ config: configPromise });
-  const locale = await getCurrentLocale();
-  const content = await payload.findGlobal({
-    slug: "mainPage",
-    locale,
-  });
   return (
     <>
       <Header text="Tietokilta 40" animated />
-      <main className="container mx-auto max-w-5xl px-8 py-8">
-        <LexicalSerializer data={content.body} />
-      </main>
+      <MainPage />
     </>
   );
 }
--- src/app/[locale]/infoscreen/page.tsx
+++ src/app/[locale]/infoscreen/page.tsx
@@ -1,20 +1,5 @@
-import { getPayload } from "payload";
-import configPromise from "@payload-config";
-import { LexicalSerializer } from "@components/lexical/LexicalSerializer";
-import { getCurrentLocale } from "@locales/server";
+import MainPage from "@components/MainPage";
 
-export default async function Home() {
-  const payload = await getPayload({ config: configPromise });
-  const locale = await getCurrentLocale();
-  const content = await payload.findGlobal({
-    slug: "mainPage",
-    locale,
-  });
-  return (
-    <>
-      <main className="container mx-auto max-w-5xl px-8 py-8">
-        <LexicalSerializer data={content.body} />
-      </main>
-    </>
-  );
+export default async function InfoscreenPage() {
+  return <MainPage />;
 }
