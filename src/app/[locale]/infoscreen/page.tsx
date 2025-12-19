1fb2eb190aeb2c2c72556ea548e12ff734a99c07
lanttu1243 <lasse.kukkula@gmail.com>
2025-12-19 17:23:41 +0200

    Fix Infoscreen page to only display events

--- src/app/[locale]/infoscreen/page.tsx
+++ src/app/[locale]/infoscreen/page.tsx
@@ -1,5 +1,16 @@
-import MainPage from "@components/MainPage";
+import Events from "@components/events/Events";
+import { getCurrentLocale, getScopedI18n } from "@locales/server";
 
 export default async function InfoscreenPage() {
-  return <MainPage />;
+  const locale = await getCurrentLocale();
+  const t = await getScopedI18n("ilmomasiina");
+
+  return (
+    <div className="h-full w-full">
+      <h1 className="font-pixel w-full pt-4 text-center text-4xl font-extrabold">
+        {t("all-events.Kaikki tapahtumat")}
+      </h1>
+      <Events locale={locale} />
+    </div>
+  );
 }
