af3331c5a912e0b87a2d0f8e0143cfcaef099552
IiroP <iiro.poutiainen@gmail.com>
2026-01-12 10:12:46 +0200

    fix: properly show quota statuses

--- src/app/components/events/QuotaWindow.tsx
+++ src/app/components/events/QuotaWindow.tsx
@@ -1,7 +1,11 @@
 import { ProgressBar } from "@components/basic/ProgressBar";
 import { Window } from "@components/Window";
 import { getScopedI18n } from "@locales/server";
-import { UserEventResponse } from "@tietokilta/ilmomasiina-models";
+import { getSignupsByQuota } from "@tietokilta/ilmomasiina-client/dist/utils/signupUtils";
+import {
+  SignupStatus,
+  UserEventResponse,
+} from "@tietokilta/ilmomasiina-models";
 
 export default async function QuotaWindow({
   event,
@@ -12,22 +16,55 @@ export default async function QuotaWindow({
 
   return (
     <Window title={t("quotas")} className="font-pixel">
-      {event.quotas.map((quota) => (
-        <div key={quota.id} className="mb-4">
-          <p>
-            {quota.title}{" "}
-            {quota.size
-              ? `(${quota.signupCount ?? 0}/${quota.size ?? 0})`
-              : `(${quota.signupCount ?? 0})`}
-          </p>
-          <ProgressBar
-            max={quota.size ?? 1}
-            value={
-              quota.size ? Math.min(quota.signupCount ?? 0, quota.size) : 0
+      {getSignupsByQuota(event).map((quota) => {
+        switch (quota.type) {
+          case SignupStatus.IN_QUOTA:
+            return (
+              <QuotaProgress
+                key={quota.id}
+                title={quota.title ?? t("headers.Kiintiö")}
+                size={quota.size}
+                signupCount={quota.signupCount}
+              />
+            );
+          case SignupStatus.IN_OPEN_QUOTA:
+            return (
+              <QuotaProgress
+                key={quota.id}
+                title={t("Avoin kiintiö")}
+                size={quota.size}
+                signupCount={quota.signupCount}
+              />
+            );
+          case SignupStatus.IN_QUEUE:
+            if (quota.signupCount > 0) {
+              return <p>{`${t("Jonossa")}: ${quota.signupCount}`}</p>;
             }
-          />
-        </div>
-      ))}
+        }
+      })}
     </Window>
   );
 }
+
+const QuotaProgress = ({
+  title,
+  size,
+  signupCount,
+}: {
+  title: string;
+  size: number | null;
+  signupCount: number | null;
+}) => {
+  return (
+    <div className="mb-4">
+      <p>
+        {title}{" "}
+        {size ? `(${signupCount ?? 0}/${size ?? 0})` : `(${signupCount ?? 0})`}
+      </p>
+      <ProgressBar
+        max={size ?? 1}
+        value={size ? Math.min(signupCount ?? 0, size) : 0}
+      />
+    </div>
+  );
+};
