6d9074740f1f0377d226709605f50a8b8b5599e0
IiroP <iiro.poutiainen@gmail.com>
2026-01-18 17:50:10 +0200

    feat: display error below signup button
    - if e.g. ilmomasiina connection fails, now it displays error message to user instead of silent fail

--- src/app/components/signup/SignUp.tsx
+++ src/app/components/signup/SignUp.tsx
@@ -1,7 +1,11 @@
 "use client";
 import { useScopedI18n } from "@locales/client";
 import { Button } from "@components/basic/Button";
-import { beginSignup, configureApi } from "@tietokilta/ilmomasiina-client";
+import {
+  ApiError,
+  beginSignup,
+  configureApi,
+} from "@tietokilta/ilmomasiina-client";
 import { useRouter } from "next/navigation";
 import { Quota } from "@tietokilta/ilmomasiina-models";
 import React, { useState } from "react";
@@ -15,27 +19,36 @@ export const SignUp = ({
   disabled: boolean;
 }) => {
   const t = useScopedI18n("ilmomasiina");
+  const t_e = useScopedI18n("errors.ilmo.code");
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<ApiError | null>(null);
   const router = useRouter();
   configureApi(ILMOMASIINA_API_BASE_URL);
 
   const signUpAction = async () => {
     setLoading(true);
     try {
+      setError(null);
       const result = await beginSignup(quota.id);
       router.push(`/signups/${result.id}/${result.editToken}`);
     } catch (error) {
+      if (error instanceof ApiError) {
+        setError(error);
+      }
       console.log("Error signing up:", error);
       setLoading(false);
     }
   };
 
   return (
-    <Button
-      text={`${t("signup")}: ${quota.title} ${quota.price > 0 ? `(${quota.price / 100} €)` : ""}`}
-      disabled={disabled || loading}
-      type="button"
-      onClick={signUpAction}
-    />
+    <div>
+      <Button
+        text={`${t("signup")}: ${quota.title} ${quota.price > 0 ? `(${quota.price / 100} €)` : ""}`}
+        disabled={disabled || loading}
+        type="button"
+        onClick={signUpAction}
+      />
+      {error?.code && <p className="text-juvu-red-dark">{t_e(error.code)}</p>}
+    </div>
   );
 };
