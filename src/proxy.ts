faf474e705407f824327871015fac12df63c61e8
lanttu1243 <lasse.kukkula@gmail.com>
2026-09-07 17:56:51 +0300

    Remove redundant domain hassle

--- src/proxy.ts
+++ src/proxy.ts
@@ -18,28 +18,6 @@ export function proxy(req: NextRequest) {
     return NextResponse.redirect(target);
   }
 
-  // When we have separate domain for anniversary year,
-  // we want muistinnollaus.fi to redirect there
-  const primaryDomain = process.env.PRIMARY_DOMAIN;
-  const hostname = req.headers.get("host")!;
-  if (
-    primaryDomain &&
-    hostname !== primaryDomain &&
-    (hostname === "muistinnollaus.fi" ||
-      hostname === "www.muistinnollaus.fi" ||
-      hostname === "localhost:3000")
-  ) {
-    // Only trigger for frontpage requests (this allows easier redirect urls)
-    if (
-      req.nextUrl.pathname === "/" ||
-      req.nextUrl.pathname === "/fi" ||
-      req.nextUrl.pathname === "/en"
-    ) {
-      return NextResponse.redirect(
-        new URL(`https://${primaryDomain}/m0`, req.url),
-      );
-    }
-  }
   return I18nMiddleware(req);
 }
 export const config = {
